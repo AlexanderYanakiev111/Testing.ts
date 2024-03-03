94f4c2942041ac958e79144765ebc37d2e3dbb79
SasTest96 <152311624+AlexanderYanakiev111@users.noreply.github.com>
2024-03-03 20:24:56 +0200

    Refactoring specialHotPage.ts

--- pages/specialHotPage.ts
+++ pages/specialHotPage.ts
@@ -2,7 +2,7 @@ import { Page } from "@playwright/test";
 
 export default class SpecialHotPage {
   
-constructor(public page: Page) {}
+ constructor(public page: Page) {}
 
    async addFirstProductToTheCart() {   
         await this.page.hover("//div[@class='image']/a", {
@@ -10,7 +10,7 @@ constructor(public page: Page) {}
         });
         await this.page.locator("(//button[@title='Add to Cart'])")
             .nth(0).click;
-    }
+  }
     
     async isToastVisible() {   
         const toast = this.page.locator("//a[.='View Cart ']");
